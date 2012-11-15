b0c9ff4715946d88599bba02b548f5b0ef75801e
Alexander Shopov <ash@kambanaria.org>
2012-11-15 07:00:53 +0200

    Added all tests to all test
    TODO: Improve names, in suite one fails
    
    Signed-off-by: Alexander Shopov <ash@kambanaria.org>

--- src/test/java/org/kambanaria/writebytecode/asm/AllTests.java
+++ src/test/java/org/kambanaria/writebytecode/asm/AllTests.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TrivialZombunnyTest.class, ZombunnyTest.class, RenameTest.class, AddFieldTest.class, AddInterfaceTest.class, ManipulateConstructors.class})
+@Suite.SuiteClasses({TrivialZombunnyTest.class, ZombunnyTest.class, RenameTest.class, AddFieldTest.class, AddInterfaceTest.class, ManipulateConstructors.class, ChangeMethodTest.class})
 public class AllTests {
 }
