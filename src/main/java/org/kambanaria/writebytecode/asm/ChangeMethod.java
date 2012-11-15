37c1940487f4ad3cc20d28abb6456ed9146a05fa
Alexander Shopov <ash@kambanaria.org>
2012-11-15 06:56:14 +0200

    Implemented change of method
    Signed-off-by: Alexander Shopov <ash@kambanaria.org>

--- src/main/java/org/kambanaria/writebytecode/asm/ChangeMethod.java
+++ src/main/java/org/kambanaria/writebytecode/asm/ChangeMethod.java
@@ -5,29 +5,37 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import static org.objectweb.asm.Opcodes.*;
 
-
 public class ChangeMethod extends DemoClassAdapter {
 
-    public ChangeMethod(ClassVisitor cv){
+    public ChangeMethod(ClassVisitor cv) {
         super(cv);
     }
-    
+
     @Override
     public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
         MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
-
-        if (Utilities.METHOD_NAME.equals(name) && "()I".equals(desc)) {
+        System.out.println(name);
+        System.out.println(desc);
+        System.out.println(Utilities.METHOD_NAME.equals(name));
+        System.out.println("()Ljava/lang/Integer".equals(desc));
+        System.out.println(Utilities.METHOD_NAME.equals(name) && "()Ljava/lang/Integer;".equals(desc));
+        if (Utilities.METHOD_NAME.equals(name) && "()Ljava/lang/Integer;".equals(desc)) {
             return new DemoMethodVisitor(Opcodes.ASM4, mv);
         } else {
             return mv;
         }
     }
-    
-    class DemoMethodVisitor extends MethodVisitor{
-        public DemoMethodVisitor(int version, MethodVisitor mv){
+
+    class DemoMethodVisitor extends MethodVisitor {
+
+        public DemoMethodVisitor(int version, MethodVisitor mv) {
             super(version, mv);
-            mv.visitIntInsn(BIPUSH, 42);
-            mv.visitInsn(IRETURN);
+            mv.visitCode();
+            mv.visitVarInsn(ALOAD, 0);
+            mv.visitFieldInsn(GETFIELD, "org/kambanaria/writebytecode/asm/Zombunny", "_version", "Ljava/lang/Integer;");
+            mv.visitInsn(ARETURN);
+            mv.visitEnd();
+
         }
     }
 }
---
+++ src/test/java/org/kambanaria/writebytecode/asm/ChangeMethodTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kambanaria.writebytecode.asm;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import org.junit.After;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+
+/**
+ *
+ * @author ashopov
+ */
+public class ChangeMethodTest {
+
+    Object sut;
+
+    @Before
+    public void setUp() throws IOException, ReflectiveOperationException {
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES + ClassWriter.COMPUTE_MAXS);
+        ClassVisitor cv = new ChangeMethod(new AddField(cw));
+        ClassReader rdr = new ClassReader(Utilities.CLASS_NAME);
+        rdr.accept(cv, 0);
+        byte[] newClassBytes = cw.toByteArray();
+        StupidClassLoader ldr = new StupidClassLoader();
+        ldr.provide(Utilities.CLASS_NAME, newClassBytes);
+        Class<?> newClass = ldr.loadClass(Utilities.CLASS_NAME);
+        sut = newClass.newInstance();
+    }
+
+    @After
+    public void tearDown() {
+        sut = null;
+    }
+
+    @Test
+    public void testChangedMethod() throws ReflectiveOperationException {
+        Method method = sut.getClass().getDeclaredMethod(Utilities.METHOD_NAME, (Class<?>[]) null);
+        Object newVersion = method.invoke(sut, (Object[]) null);
+        assertEquals(null, newVersion);
+    }
+
+}
